72d4364b472c52688cf04e0b372d66fdb9bdaa7d
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2018-08-22 19:49:44 -0400

    adds gitignore

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 // Est. the port
-var PORT = 3000;
+var PORT = 3001;
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // MAKE DA CONNECTION!!!!! to the Mongo DB
-mongoose.connect("mongodb://localhost/scraper"); //// not sure what needs to go here????
+mongoose.connect("mongodb://localhost/nbcscraper"); //// not sure what needs to go here????
 
 ///////////////////// ROUTES ///////////////////////////
 // A GET route for scraping the nbc news website
@@ -39,7 +39,7 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // grab every <a> and its href attribute within an <h1> with the class title__1T2jw
-    $("h1.title__1T2jw").each(function(i, element) {
+    $("h1.title___1T2jw").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
@@ -51,77 +51,81 @@ app.get("/scrape", function(req, res) {
         .children("a")
         .attr("href");
 
-        
+      console.log( "RESULT FROM SCRAPING", result );
 
+      // Creating new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
 
- // Creating new Article using the `result` object built from scraping
- db.Article.create(result)
- .then(function(dbArticle) {
-   // View the added result in the console
-   console.log(dbArticle);
- })
- .catch(function(err) {
-   // If an error occurred, send it to the client
-   return res.json(err);
- });
-});
+    // If scrape is successful, send a message to the client
+    res.send("Scrape Complete");
 
-// If scrape is successful, send a message to the client
-res.send("Scrape Complete");
-});
+  });
 });
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function(dbArticle) {
-        // If successful, send back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If successful, send back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-  // Route for grabbing a specific Article by id, populate it with it's note
+// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-    // Using the id passed in the id parameter, can make a query that finds the matching one in the db...
-    db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If successful find an Article with the given id and send it back
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+  // Using the id passed in the id parameter, can make a query that finds the matching one in the db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If successful find an Article with the given id and send it back
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-  // Route for saving/updating an Article's associated Note
+// Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-    // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
-  // Start the server
+// Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+  console.log("App running on port " + PORT + "!");
+});
