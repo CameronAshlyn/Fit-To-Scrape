f4a24e61a29b989634281814b1045c8bd599944e
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2018-08-22 20:21:47 -0400

    connects MongoDB?

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ app.use(express.static("public"));
 // MAKE DA CONNECTION!!!!! to the Mongo DB
 mongoose.connect("mongodb://localhost/nbcscraper"); //// not sure what needs to go here????
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 ///////////////////// ROUTES ///////////////////////////
 // A GET route for scraping the nbc news website
 app.get("/scrape", function(req, res) {
